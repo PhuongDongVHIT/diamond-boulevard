38c2f2a7ce09e6ba8065ee432fa546321d8cb0f5
Phuong Dong <46783682+PhuongDongVHIT@users.noreply.github.com>
2026-03-22 14:00:21 +0700

    feat: fix image

--- src/lib/actions/load-images.ts
+++ src/lib/actions/load-images.ts
@@ -1,25 +1,27 @@
-const modules = import.meta.glob('/static/images/*.webp', { eager: true, query: '?url', import: 'default' });
+const modules = import.meta.glob('../../../static/images/*.webp', { eager: true, query: '?url', import: 'default' });
 
 export const images = Object.entries(modules).map(([path, url]) => {
+  const filename = path.split('/').pop() || '';
   return {
-    src: typeof url === 'string' ? url : path.replace('/static', ''),
-    title: path.split('/').pop()?.replace('.webp', '') || '',
-    category: path.includes('mat-bang')
+    src: `/images/${filename}`,
+    title: filename.replace('.webp', '') || '',
+    category: filename.includes('mat-bang')
       ? 'mat-bang'
-      : path.includes('vi-tri')
+      : filename.includes('vi-tri')
       ? 'vi-tri'
-      : path.includes('tien-ich')
+      : filename.includes('tien-ich')
       ? 'tien-ich'
       : 'can-ho',
   };
 });
 
-const modulesLaw = import.meta.glob('/static/laws/*.webp', { eager: true, query: '?url', import: 'default' });
+const modulesLaw = import.meta.glob('../../../static/laws/*.webp', { eager: true, query: '?url', import: 'default' });
 
 export const imageLaws = Object.entries(modulesLaw).map(([path, url]) => {
+  const filename = path.split('/').pop() || '';
   return {
-    src: typeof url === 'string' ? url : path.replace('/static', ''),
-    title: path.split('/').pop()?.replace('.webp', '') || '',
+    src: `/laws/${filename}`,
+    title: filename.replace('.webp', '') || '',
   };
 });
 
