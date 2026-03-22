c550f5e1f705ca187ed0b9e58628a9096f621c41
Phuong Dong <46783682+PhuongDongVHIT@users.noreply.github.com>
2026-03-22 14:17:07 +0700

    feat: check

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
   import '../app.css';
-  import Header from '$lib/components/Header.svelte';
+  // import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
-    import Contact from '$lib/components/Contact.svelte';
+    // import Contact from '$lib/components/Contact.svelte';
   
   let { children } = $props();
 </script>
 
-<Header />
+<!-- <Header /> -->
 
 <main>
   {@render children()}
